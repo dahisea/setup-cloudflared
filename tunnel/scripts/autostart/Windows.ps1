1e3b0f90d0cf9deb2bba20f0b3a32417e3eacb05
Dahi <81472122+dahisea@users.noreply.github.com>
2025-09-16 02:24:53 +0800

    更新 Windows.ps1

--- tunnel/scripts/autostart/Windows.ps1
+++ tunnel/scripts/autostart/Windows.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Autostarting cloudflared for Windows
 if ($env:url -eq 'false') {
-  Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run"
+  Start-Process "cloudflared" "--protocol=http2 --pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run"
 }
 else {
-  Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel --url $env:url"
+  Start-Process "cloudflared" "--protocol=http2 --pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel --url $env:url"
 }
 Write-Host ::endgroup::
