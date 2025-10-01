d9f10aa4aa273fa037a3e6a9efba835d251b59c6
Dahi <81472122+dahisea@users.noreply.github.com>
2025-10-01 20:06:43 +0800

    更新 Windows.ps1

--- tunnel/scripts/autostart/Windows.ps1
+++ tunnel/scripts/autostart/Windows.ps1
@@ -5,6 +5,6 @@ if ($env:url -eq 'false') {
   Start-Process "cloudflared" "--protocol=quic --pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run"
 }
 else {
-  Start-Process "cloudflared" "--protocol=http2 --pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel --url $env:url"
+  Start-Process "cloudflared" "--protocol=quic --pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel --url $env:url"
 }
 Write-Host ::endgroup::
